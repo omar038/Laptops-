59669c116b5bbac9acd7dc5d17c89aca2b8b47b8
omar mohamed salama <68208424+omar038@users.noreply.github.com>
2024-03-19 16:46:14 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,8 @@ import streamlit as st
 from st_pages import Page, Section, show_pages, add_page_title, hide_pages
 import plotly.express as px
 import plotly.graph_objects as go
-st.set_page_config(layout="wide" ,page_title='Data Overview',page_icon="🧑‍💻")
 
+add_page_title()
 show_pages(
     [   
         Page("Home.py", "Home", "💻"),
